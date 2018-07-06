105d1f8658cc7620b1ae0b94b2c1e7f5cd665544
joyce <joycedelatorre87@gmail.com>
2018-07-06 13:43:47 -0500

    initializing javascript, modifying css to become responsive

---
+++ racer.js
@@ -0,0 +1,19 @@
+var players =[
+	{
+		id:1,
+		position:0,
+		keycode:80,
+		location:'player1'
+	},
+
+	{
+		id:2,
+		position:0,
+		keycode:81,
+		location:'player2'
+
+	}
+]
+$(document).ready(function(){
+	alert("hello world");
+});
