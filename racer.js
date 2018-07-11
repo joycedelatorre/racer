ff8fccc07a35829c0804e57d45559abfee9c2ec0
joyce <joycedelatorre87@gmail.com>
2018-07-10 19:02:09 -0500

    test

--- racer.js
+++ racer.js
@@ -14,6 +14,30 @@ var players =[
 
 	}
 ]
-$(document).ready(function(){
-	alert("hello world");
+$(document).ready(function() {
+  var end_of_track = 15;
+  
+  function advancePlayer(keyCode) {
+
+    players.forEach(function(player){
+      if (keyCode == player.keycode && players[0].position < end_of_track && players[1].position < end_of_track) {
+        move_player(player.location);
+        player.position++;
+        winner(player.position, player.id);
+      }
+    });
+  }
+
+  $(document).keyup(function(e) {
+    advancePlayer(e.keyCode);
+  });
 });
+
+function move_player(strip) {
+  $(strip).find('td.active').removeClass('active').next().addClass('active');
+}
+
+function winner(player, num) {
+  if (player > 14) {
+    alert("Player " + num + " has won!");
+  }
