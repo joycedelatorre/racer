d841dfb70c9ccd6af1e9da7f762c3f9800c464ac
joyce <joycedelatorre87@gmail.com>
2018-07-10 22:34:08 -0500

    able to log number of hit

--- racer.js
+++ racer.js
@@ -1,4 +1,6 @@
-var players =[
+
+  var end_of_track = 15;
+  var players =[
 	{
 		id:1,
 		position:0,
@@ -15,23 +17,22 @@ var players =[
 	}
 ]
 $(document).ready(function() {
-  var end_of_track = 15;
-  
-  function advancePlayer(keyCode) {
 
-    players.forEach(function(player){
-      if (keyCode == player.keycode && players[0].position < end_of_track && players[1].position < end_of_track) {
-        move_player(player.location);
-        player.position++;
-        winner(player.position, player.id);
-      }
-    });
-  }
-
-  $(document).keyup(function(e) {
-    advancePlayer(e.keyCode);
+  $(document).keypress(function(event) {
+    advancePlayer(event.keyCode);
   });
 });
+
+function advancePlayer(keyCode) {
+		console.log("hello");
+  	for(var i = 0; i < players.length; i++){
+  		if (keyCode == players[i].keycode && players[0].position < end_of_track && players[1].position < end_of_track) {
+        move_player(players.location);
+        players.position++;
+        winner(players.position, players.id);
+      }
+  	}
+ }
 
 function move_player(strip) {
   $(strip).find('td.active').removeClass('active').next().addClass('active');
@@ -41,3 +42,4 @@ function winner(player, num) {
   if (player > 14) {
     alert("Player " + num + " has won!");
   }
+ }
